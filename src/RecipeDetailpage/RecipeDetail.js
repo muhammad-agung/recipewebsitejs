dea1fcbbf74036842fe7d7168746cc018d39230b
muhammad-agung <muhdagung255@gmail.com>
2024-04-17 18:13:49 +0800

    chnage editor to tinyMCE

--- src/RecipeDetailpage/RecipeDetail.js
+++ src/RecipeDetailpage/RecipeDetail.js
@@ -1,32 +1,43 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import { Editor } from 'react-draft-wysiwyg';    
-import '../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import { EditorState, convertFromRaw } from 'draft-js';
+import { Editor } from '@tinymce/tinymce-react';
 import Box from '@mui/material/Box';
 
 const RecipeDetail = () => {
   const location = useLocation();
   const currentRecipe = location.state && location.state.currentRecipe;
 
+    // Determine the width percentage based on the display width
+    const widthPercentage = window.innerWidth > 1000 ? '70%' : '100%';
+
   if (!currentRecipe) {
     return <div>Recipe not found</div>;
   }
 
-  const contentState = convertFromRaw(JSON.parse(currentRecipe.content));
-  const editorState = EditorState.createWithContent(contentState);
-
   return (
-    <div style={{ width: '100%', backgroundColor: '#FFCCBC', paddingTop: '1%' }}>
-      <h1 style={{ textAlign: 'center', marginBottom: '20px' }}>{currentRecipe.title}</h1>
-      <p style={{ textAlign: 'center', marginBottom: '20px' }}>{currentRecipe.shortDesc}</p>
-      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+    <div style={{ width: '100%', backgroundColor: '#FFCCBC', paddingBottom: '45px' }}>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: '1%',
+        }}
+      >
+        <h1>{currentRecipe.title}</h1>
+        <p>{currentRecipe.shortDesc}</p>
         <Editor
-          wrapperStyle={{ width: '90%', maxWidth: '800px', backgroundColor: 'white' }}
-          editorState={editorState}
-          readOnly
-          toolbarHidden
-          autoHeight
+          apiKey="cnra8bfoc01172abmclve5xtbay0i4b7q9lb7hq5qj977oem" // Replace with your TinyMCE API key
+          init={{
+            menubar: false,
+            statusbar: false,
+            toolbar: false,
+            width: widthPercentage,
+            height: 800,
+          }}
+          initialValue={currentRecipe.content} // Pass the content as initialValue
+          disabled
         />
       </Box>
     </div>
