c707654687bcfede042d01720354a7c42e765ebf
muhammad-agung <muhdagung255@gmail.com>
2024-04-24 17:24:33 +0800

    minor fix

--- src/Mainpage/RecipeList.js
+++ src/Mainpage/RecipeList.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import RecipeCard from './RecipeCard';
 import Grid from '@mui/material/Unstable_Grid2';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
 import AdsComponent from '../Components/Ads/AdsComponent';
 
 
@@ -20,22 +18,5 @@ export default function ActionAreaCard({ recipes }) {
         </React.Fragment>
       ))}
     </Grid>
-  );
-}
-
-function AdSlot() {
-  return (
-    <Card sx={{ height: 400, display: 'flex', justifyContent: 'center', alignItems: 'center', boxShadow: 3 }}>
-      <CardContent>
-        {/* Your AdSense code */}
-        <ins
-            className="adsbygoogle"
-            style={{ display: 'inline-block', width: '400px', height: '400px' }}
-            data-ad-client="ca-pub-8683472106710311"
-            data-ad-slot="6779483922"
-          ></ins>
-        <script>{`(adsbygoogle = window.adsbygoogle || []).push({});`}</script>
-      </CardContent>
-    </Card>
   );
 }
