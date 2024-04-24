0b86aa4023b160df89baabe62620f998c459ccdc
muhammad-agung <muhdagung255@gmail.com>
2024-04-24 17:51:42 +0800

    remove ads for now

--- src/Mainpage/RecipeList.js
+++ src/Mainpage/RecipeList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import RecipeCard from './RecipeCard';
 import Grid from '@mui/material/Unstable_Grid2';
-import AdsComponent from '../Components/Ads/AdsComponent';
+// import AdsComponent from '../Components/Ads/AdsComponent';
 
 
 
@@ -11,7 +11,7 @@ export default function ActionAreaCard({ recipes }) {
     <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} minHeight={400} display="flex" justifyContent="center" alignItems="center" width={'100%'}>
       {recipes.map((recipe, index) => (
         <React.Fragment key={index}>
-          {(index + 2) % 3 === 0 && index !== recipes.length - 1 && <AdsComponent />}
+          {/* {(index + 2) % 3 === 0 && index !== recipes.length - 1 && <AdsComponent />} */}
           <Grid item xs={3} sm={3} md={3} display="flex" justifyContent="center" alignItems="center">
           <RecipeCard key={recipe.id} recipe={recipe} />
           </Grid>
